a4c6b99426fba7211d24d9e4d2686947d3d8dc9c
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-03-01 12:12:52 +0500

    add

---
+++ src/pages/auth/Auth.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from './Login'
+import Register from './Register'
+
+export default function Auth() {
+  return (
+    <div className='Auth'>
+        <Login/>
+        <Register/>
+    </div>
+  )
+}
