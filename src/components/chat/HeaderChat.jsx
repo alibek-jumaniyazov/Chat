8ec1ede80461f7a1afd370ef84a86157d3415fb0
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-03-02 15:55:50 +0500

    add

--- src/components/chat/HeaderChat.jsx
+++ src/components/chat/HeaderChat.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import userImg from '../../assets/images/userImg.png'
+
 
 export default function HeaderChat() {
   return (
     <div className='HeaderChat'>
-        
+          <div className="userImg">
+            <img src={userImg} alt="" />
+        </div>
+        <div className="userTitle">
+            <p className="userName">John Doe</p>
+            <span className="userSet">online</span>
+        </div>
     </div>
   )
 }
--- src/components/chat/UserPostMessage.jsx
+++ src/components/chat/UserPostMessage.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Button, Input, Space } from 'antd'
+import {  LinkOutlined } from '@ant-design/icons';
 
 export default function UserPostMessage() {
+
+
   return (
     <div className='UserPostMessage'>
-
+      <LinkOutlined className='userPostMessange__file'/>
+      <Space.Compact style={{ width: '100%', height:'50px' }}>
+        <Input defaultValue="Combine input and button" />
+        <Button type="primary" style={{ height:'50px' }}>Submit</Button>
+      </Space.Compact>
     </div>
   )
 }
